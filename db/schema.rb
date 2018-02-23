b4e2828c359b5432a18adc8cdddb24df2ba09555
PLSV <greasemonkey12394@gmail.com>
2018-02-23 23:07:22 +0530

    adding authentication through passwords

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222162101) do
+ActiveRecord::Schema.define(version: 20180223172809) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180222162101) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
